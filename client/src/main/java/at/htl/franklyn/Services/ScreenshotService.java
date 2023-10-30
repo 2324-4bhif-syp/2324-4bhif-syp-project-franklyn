b89056546f8212e1cc69ba8c3b999db0de67b4bf
LukaCvijic <lukacvijic@outlook.com>
2023-10-30 16:52:51 +0100

    Added method where screenshot can be scaled

--- client/src/main/java/at/htl/franklyn/Services/ScreenshotService.java
+++ client/src/main/java/at/htl/franklyn/Services/ScreenshotService.java
@@ -8,7 +8,7 @@ public class ScreenshotService {
     public static BufferedImage getScreenshot(){
 
         BufferedImage screenShot;
-        
+
         try {
             screenShot = new Robot().createScreenCapture(
                     new Rectangle(Toolkit.getDefaultToolkit().getScreenSize())
@@ -17,6 +17,17 @@ public class ScreenshotService {
         } catch (AWTException e) {
             throw new RuntimeException(e);
         }
+
+        return screenShot;
+    }
+
+    public static BufferedImage getScreenshot(int width, int height){
+
+        BufferedImage screenShot = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        Image scaledImage = getScreenshot().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+
+        screenShot.getGraphics().drawImage(scaledImage, 0, 0, null);
 
         return screenShot;
     }
