d060ec7cbf01d806a2c754837d177464b46809f9
LukaCvijic <lukacvijic@outlook.com>
2023-10-30 14:05:39 +0100

    Added screenshotService to get screen

---
+++ client/src/main/java/at/htl/franklyn/Services/ScreenshotService.java
@@ -0,0 +1,23 @@
+package at.htl.franklyn.Services;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ScreenshotService {
+
+    public static BufferedImage getScreenshot(){
+
+        BufferedImage screenShot;
+        
+        try {
+            screenShot = new Robot().createScreenCapture(
+                    new Rectangle(Toolkit.getDefaultToolkit().getScreenSize())
+            );
+
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+
+        return screenShot;
+    }
+}
