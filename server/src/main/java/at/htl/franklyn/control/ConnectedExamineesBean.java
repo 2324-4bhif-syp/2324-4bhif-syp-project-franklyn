01844fc69e452bf61ac3bdd2bc21c771914ecf9f
HikariTempest <arwed@gmx.at>
2023-11-18 17:27:34 +0100

    feat: added properties for the scheduled task of the server #244bfk-30 To Verify

--- server/src/main/java/at/htl/franklyn/control/ConnectedExamineesBean.java
+++ server/src/main/java/at/htl/franklyn/control/ConnectedExamineesBean.java
@@ -4,20 +4,21 @@ import io.quarkus.logging.Log;
 import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 
 @ApplicationScoped
-public class ConnectedExaminesBean {
+public class ConnectedExamineesBean {
     @Inject
     ExamineeConnectionRepository examineeConnectionRepository;
 
-    private static final long MAX_LIVE_TIME_OF_CONNECTION_IN_SECONDS = 120;
-    private static final String SCHEDULER_TIME = MAX_LIVE_TIME_OF_CONNECTION_IN_SECONDS + "s";
+    @ConfigProperty(name = "connectedexamineesbean.lifetime")
+    String MAX_LIVE_TIME_OF_CONNECTION_IN_SECONDS;
 
-    @Scheduled(every = SCHEDULER_TIME)
+    @Scheduled(every = "{connectedexamineesbean.interval}")
     public void removeExpiredExamineeConnections() {
-        List<String> expiredExamineIpAdress = examineeConnectionRepository.getAllExpiredExamines(MAX_LIVE_TIME_OF_CONNECTION_IN_SECONDS);
+        List<String> expiredExamineIpAdress = examineeConnectionRepository.getAllExpiredExamines(Long.parseLong(MAX_LIVE_TIME_OF_CONNECTION_IN_SECONDS));
 
         for (String ipAdress : expiredExamineIpAdress) {
             examineeConnectionRepository.removeConnection(ipAdress);
