bef8267271ca161670c5ce5f4413aed1b0adb7f8
aichingert <aichinger.tobias13@gmail.com>
2023-11-20 14:05:26 +0100

    feat: added quarkus rest client for instructor to get all current examinees #244bfk-28

--- instructor-client/src/main/java/at/htl/franklyn/boundary/SomePage.java
+++ instructor-client/src/main/java/at/htl/franklyn/boundary/SomePage.java
@@ -1,18 +1,25 @@
-package at.htl.franklyn.services;
+package at.htl.franklyn.boundary;
 
+import at.htl.franklyn.services.ExamineesService;
+import io.quarkus.logging.Log;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import static java.util.Objects.requireNonNull;
 
 @Path("/some-page")
 public class SomePage {
+    @Inject
+    @RestClient
+    ExamineesService examineesService;
 
     private final Template page;
 
@@ -22,8 +29,12 @@ public class SomePage {
 
     @GET
     @Produces(MediaType.TEXT_HTML)
+    @io.smallrye.common.annotation.Blocking
     public TemplateInstance get(@QueryParam("name") String name) {
-        return page.data("name", name);
+        // Example usage of examineesService:
+
+        Log.info(examineesService.getExaminees());
+        return page.data("name", examineesService.getExaminees().get(0));
     }
 
 }
