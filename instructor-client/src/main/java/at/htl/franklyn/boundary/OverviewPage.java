41461dd01202e985b38c29bac8dbe26acb5c8096
aichingert <aichinger.tobias13@gmail.com>
2023-11-21 10:57:52 +0100

    feat: polishing

--- instructor-client/src/main/java/at/htl/franklyn/boundary/OverviewPage.java
+++ instructor-client/src/main/java/at/htl/franklyn/boundary/OverviewPage.java
@@ -1,15 +1,18 @@
 package at.htl.franklyn.boundary;
 
 import at.htl.franklyn.services.ExamineesService;
+import at.htl.franklyn.services.ScreenshotService;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.net.URI;
+import java.util.Base64;
 
 import static java.util.Objects.requireNonNull;
 
@@ -26,11 +29,23 @@ public class OverviewPage {
     }
 
     @GET
+    @Path("screenshot/{ip}")
+    @Produces(MediaType.TEXT_HTML)
+    @io.smallrye.common.annotation.Blocking
+    public TemplateInstance updateScreenshot(@PathParam("ip") String ip) {
+        ScreenshotService screenshotService = RestClientBuilder.newBuilder()
+                .baseUri(URI.create(String.format("http://%s:8080", ip)))
+                .build(ScreenshotService.class);
+
+        return this.overview.data("examinees", examineesService.getExaminees())
+                .data("image", Base64.getEncoder().encodeToString(screenshotService.getScreenshot()));
+    }
+
+    @GET
     @Produces(MediaType.TEXT_HTML)
     @io.smallrye.common.annotation.Blocking
     public TemplateInstance get() {
-
-        return overview.data("examinees", examineesService.getExaminees());
+        return overview.data("examinees", examineesService.getExaminees())
+                .data("image", "");
     }
-
 }
