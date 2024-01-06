63e55eb35b9a85d5fd1fdf5e5c4819905225655f
HikariTempest <arwed@gmx.at>
2024-01-06 19:20:40 +0100

    feat: Now there is a .env file which can be edited to change the values and if one starts the application in normal mode (java -jar something-runner.jar) one gets asked to write their name into the console

---
+++ openbox/src/main/java/at/htl/franklyn/control/AppLifecycleBean.java
@@ -0,0 +1,28 @@
+package at.htl.franklyn.control;
+
+import at.htl.franklyn.services.UserService;
+import io.quarkus.logging.Log;
+import io.quarkus.runtime.LaunchMode;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class AppLifecycleBean {
+    @Inject
+    UserService userService;
+
+    void onStart(@Observes StartupEvent ev) {
+        if (LaunchMode.current() == LaunchMode.NORMAL) {
+            Log.info("Enter your name: ");
+            String name = System.console().readLine();
+
+            if (!name.isBlank()) {
+                userService.setUserName(name);
+                Log.info("Username: " + userService.getUserName() + "\n");
+            }
+        }
+    }
+
+}
