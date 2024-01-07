4403a2d0740771eb330b43168c62077472a1be6a
aichingert <91525610+aichingert@users.noreply.github.com>
2024-01-07 19:19:58 +0100

    Update instructor-client/src/app/shared/repository/examinee-dto-filter.service.ts

--- instructor-client/src/app/shared/repository/examinee-dto-filter.service.ts
+++ instructor-client/src/app/shared/repository/examinee-dto-filter.service.ts
@@ -13,8 +13,6 @@ export class ExamineeDtoFilterService {
   constructor(private webApi: WebApiService) {}
 
   public determineIpForExaminees(examineeDto: ExamineeDto) {
-    // TODO/NOTE: If the frontend is started after the client is connected and then disconnects
-    //
     for (const ip of examineeDto.ipAddresses) {
       this.webApi.getOpenBoxImage(ip).subscribe({
           next: () => this.examinee.next(new Examinee(examineeDto.username, ip, examineeDto.connected)),
