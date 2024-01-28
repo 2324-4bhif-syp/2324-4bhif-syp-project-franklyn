9281ce1657e4d8f00c69959a2bb46b956c1aed7a
LukaCvijic <lukacvijic@outlook.com>
2024-01-28 16:24:46 +0100

    feat: Added 2 endpoints which either download the video for one student or download all videos for every student and send it as zip file 244bfk-72 Done

--- recorder/src/main/java/at/htl/franklyn/boundary/VideoResource.java
+++ recorder/src/main/java/at/htl/franklyn/boundary/VideoResource.java
@@ -8,8 +8,11 @@ import jakarta.ws.rs.Produces;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.bytedeco.ffmpeg.global.avcodec;
 import org.bytedeco.javacv.FFmpegFrameRecorder;
@@ -21,11 +24,67 @@ import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
 @Path("/video")
 public class VideoResource {
 
+    @Path("/download")
+    @Produces("file/zip")
+    @GET
+    public File downloadAllVideos(){
+        try {
+            // Parent-folder
+            File screenshotFolder = new File("screenshots");
+            // Input/Output-folder
+            File[] targetDirectories = screenshotFolder.listFiles();
+
+            // Return if folder with this user does not exist
+            if(targetDirectories == null || targetDirectories.length == 0 ){
+                return null;
+            }
+
+            FileOutputStream fos = new FileOutputStream("screenshots/compressed.zip");
+            ZipOutputStream zipOut = new ZipOutputStream(fos);
+
+            List<File> allVideos = new ArrayList<File>();
+
+            for(File files : targetDirectories){
+                String[] fileName = files.getName().split("-");
+
+                if(fileName.length != 2){
+                    continue;
+                }
+
+                File fileToZip = getVideo(fileName[0]);
+                FileInputStream fis = new FileInputStream(fileToZip);
+                ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+                zipOut.putNextEntry(zipEntry);
+
+                byte[] bytes = new byte[1024];
+                int length;
+                while((length = fis.read(bytes)) >= 0) {
+                    zipOut.write(bytes, 0, length);
+                }
+                fis.close();
+            }
+
+            zipOut.close();
+            fos.close();
+
+            return new File("screenshots/compressed.zip");
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Path("/download/{ip}")
+    @Produces("video/avi")
+    @GET
+    public File downloadOneVideo(@PathParam("ip") String ip){
+        return getVideo(ip);
+    }
 
     @Path("/{ip}")
     @Produces("video/mp4")
     @GET
-    public File getScreenshot(@PathParam("ip") String ip) {
+    public File getVideo(@PathParam("ip") String ip) {
 
         try {
             // Parent-folder
@@ -52,29 +111,30 @@ public class VideoResource {
             Arrays.sort(screenshots);
 
             // Set path and size for recorder
-            FFmpegFrameRecorder recorder = getRecorder(screenshots[0], targetDirectory);
+            FFmpegFrameRecorder recorder = getRecorder(screenshots[0], targetDirectory, ip);
 
             // record images
             record(recorder, screenshots);
 
-            return new File(String.format("%s/video.mp4", targetDirectory.getPath()));
+            return new File(String.format("%s/%s.mp4", targetDirectory.getPath(), ip));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private FFmpegFrameRecorder getRecorder(File sample, File targetDirectory) throws IOException{
+    private FFmpegFrameRecorder getRecorder(File sample, File targetDirectory, String ip) throws IOException{
         // Get just one image to set width and height
         BufferedImage test = ImageIO.read(sample);
 
         // Set path and size for recorder
         FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(
-                String.format("%s/video.mp4", targetDirectory.getPath()),
+                String.format("%s/%s.mp4", targetDirectory.getPath(), ip),
                 test.getWidth(),
                 test.getHeight()
         );
 
         // more settings
+        //recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
         recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
         recorder.setFormat("mp4");
         recorder.setFrameRate(1);
