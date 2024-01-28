c3e82eeb6bd01b9098922b61e21e6fa9765e1da5
ICQun <jln.schludermann@gmail.com>
2024-01-28 17:16:57 +0100

    fix: give github variables via parameters to setup-ssh
    - #244bfk-71

--- cicd/setup-ssh.sh
+++ cicd/setup-ssh.sh
@@ -3,9 +3,9 @@
 set -e
 
 mkdir -p ~/.ssh
-echo "$SSH_PRIVATE_KEY" > ${{github.workspace}}/private.key
-sudo chmod 600 ${{github.workspace}}/private.key
+echo "$SSH_PRIVATE_KEY" > $1/private.key
+sudo chmod 600 $1/private.key
 echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
-echo "SSH_KEY_PATH=${{github.workspace}}/private.key" >> "$GITHUB_ENV"
-echo "SSH_USER=${{secrets.SSH_USERNAME}}" >> "$GITHUB_ENV"
-echo "SSH_HOST=${{secrets.SSH_HOST}}" >> "$GITHUB_ENV"
+echo "SSH_KEY_PATH=$1/private.key" >> "$GITHUB_ENV"
+echo "SSH_USER=$2" >> "$GITHUB_ENV"
+echo "SSH_HOST=$3" >> "$GITHUB_ENV"
