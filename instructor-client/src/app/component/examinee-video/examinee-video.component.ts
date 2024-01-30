a7dea4cc52f194709233098584ff539767517372
HikariTempest <arwed@gmx.at>
2024-01-30 09:04:38 +0100

    fix: now the thumbnail is also updated when switching to another examinee in the video viewer

--- instructor-client/src/app/component/examinee-video/examinee-video.component.ts
+++ instructor-client/src/app/component/examinee-video/examinee-video.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Examinee} from "../../shared/entity/Examinee";
 import {CommonModule} from "@angular/common";
 import {environment} from "../../../../env/environment";
+import ExamineeDataService from "../../shared/repository/examinee-data.service";
 
 @Component({
   selector: 'app-examinee-video',
@@ -11,9 +12,16 @@ import {environment} from "../../../../env/environment";
   styleUrl: './examinee-video.component.css'
 })
 export class ExamineeVideoComponent{
+  constructor(private examineeRepo: ExamineeDataService) {
+  }
+
   @Input() examinee: Examinee | undefined;
 
   getVideoUrl(): string {
     return `${environment.recorderBaseUrl}video/${this.examinee?.ipAddress}`; //examinee gets checked in the html
+  }
+
+  showVideo():boolean {
+    return this.examinee !== undefined && this.examineeRepo.getPatrolExaminee()?.ipAddress === this.examinee.ipAddress;
   }
 }
