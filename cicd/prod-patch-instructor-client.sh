b8a4de874d985ba71b706d51820ee1fd049f9e54
ICQun <jln.schludermann@gmail.com>
2024-02-24 12:35:00 +0100

    fix [CI]: adapt ci to new architecture
    - Remove recorder code
    
    - Extend prod patches

--- cicd/prod-patch-instructor-client.sh
+++ cicd/prod-patch-instructor-client.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-sed -i "s/serverBaseUrl.*/serverBaseUrl: '\/api\/examinees',/g" instructor-client/env/environment.ts
+sed -i "s/serverBaseUrl.*/serverBaseUrl: '\/api',/g" instructor-client/env/environment.ts
 
