09cdfa1a8a7d47af0a8039956633d347e4c45a83
icqun <jln.schludermann@gmail.com>
2024-03-10 15:14:54 +0100

    feat [instructor-client]: simplfy production base-url

--- instructor-client/env/environment.production.ts
+++ instructor-client/env/environment.production.ts
@@ -2,7 +2,7 @@ export const environment = {
   nextClientScheduleTime: 10,
   minNextClientScheduleTime: 1,
   maxNextClientScheduleTime: 30,
-  serverBaseUrl: "https://franklyn.ddns.net/api",
+  serverBaseUrl: "/api",
   imageWidth: 1920,
   imageHeight: 1080,
 }
