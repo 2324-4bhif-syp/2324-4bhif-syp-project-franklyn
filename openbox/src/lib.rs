6646a7e7228d560945f6987cd3f4ae1199e2b5ed
aichingert <aichinger.tobias13@gmail.com>
2024-05-03 22:10:51 +0200

    feat: finished ui with websocket and screenshot
     * partial wayland support gnome and kde are still tricky
    
    #244bfk-128

--- openbox/src/lib.rs
+++ openbox/src/lib.rs
@@ -8,8 +8,29 @@ use http_body_util::Empty;
 use hyper::{header::{CONNECTION, UPGRADE}, upgrade::Upgraded, Request};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpStream;
+use iced::subscription::{self, Subscription};
+use futures::channel::mpsc;
+use futures::sink::SinkExt;
 
 const DOMAIN: &str = "localhost:8080";
+
+#[derive(Debug, Clone)]
+pub struct Data {
+    pub code: u32,
+    pub lastname: String,
+    pub firstname: String,
+}
+
+enum State {
+    Disconnected,
+    Connected(FragmentCollector<TokioIo<Upgraded>>),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Event {
+    Connected,
+    Disconnected,
+}
 
 struct SpawnExecutor;
 
@@ -23,7 +44,84 @@ where
     }
 }
 
-pub async fn connect(path: &str) -> Result<FragmentCollector<TokioIo<Upgraded>>> {
+pub fn connect(Data { code, lastname, firstname }: &Data) -> Subscription<Event> {
+    let _code = code;
+    let path = format!("examinee/{firstname}_{lastname}");
+    struct Connect;
+
+    let ws_closure = |mut output: mpsc::Sender<_>| async move {
+        let mut state = State::Disconnected;
+
+        loop {
+            match &mut state {
+                State::Disconnected => {
+                    if let Ok(ws) = connect_ws(&path).await {
+                        _ = output.send(Event::Connected).await;
+                        state = State::Connected(ws);
+                    } else {
+                        println!("Failed");
+                        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                        _ = output.send(Event::Disconnected).await;
+                    }
+                }
+                State::Connected(ref mut ws) => {
+                    if handle_msg(ws).await.is_ok_and(|k| !k) {
+                        _ = output.send(Event::Disconnected).await;
+                        state = State::Disconnected;
+                    }
+                }
+            }
+        }
+    };
+
+    subscription::channel(std::any::TypeId::of::<Connect>(), 100, ws_closure)
+}
+
+pub async fn handle_msg(ws: &mut FragmentCollector<TokioIo<Upgraded>>) -> Result<bool> {
+    let msg = match ws.read_frame().await {
+        Ok(msg) => msg,
+        Err(e)  => {
+            println!("Error: {}", e);
+            _ = ws.write_frame(Frame::close_raw(vec![].into())).await?;
+            return Ok(false);
+        }
+    };
+
+    match msg.opcode {
+        OpCode::Text | OpCode::Binary => {
+            println!("{:?}", msg.payload);
+            let mut buf = Vec::<u8>::new();
+
+            xcap::Monitor::all().unwrap()
+                .first().unwrap()
+                .capture_image().unwrap()
+                .write_to(
+                    &mut std::io::Cursor::new(&mut buf),
+                    image::ImageFormat::Png,
+                )?;
+
+            let file_part = reqwest::multipart::Part::bytes(buf)
+                .file_name("image.png")
+                .mime_str("image/png")
+                .unwrap();
+
+            let form = reqwest::multipart::Form::new().part("image", file_part);
+
+            let res = reqwest::Client::new().post("http://localhost:8080/screenshot/tobias/alpha")
+                .multipart(form)
+                .send()
+                .await?;
+
+            println!("{res:?}");
+        }
+        OpCode::Close => return Ok(false),
+        _ => (),
+    }
+
+    Ok(true)
+}
+
+pub async fn connect_ws(path: &str) -> Result<FragmentCollector<TokioIo<Upgraded>>> {
     let stream = TcpStream::connect(DOMAIN).await?;
 
     let req = Request::builder()
@@ -43,57 +141,14 @@ pub async fn connect(path: &str) -> Result<FragmentCollector<TokioIo<Upgraded>>>
     Ok(FragmentCollector::new(ws))
 }
 
-
-/*
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
-    let mut ws = connect("examinee/tobias").await?;
-    let client = reqwest::Client::new();
-
-    loop {
-        let msg = match ws.read_frame().await {
-            Ok(msg) => msg,
-            Err(e)  => {
-                println!("Error: {}", e);
-                ws.write_frame(Frame::close_raw(vec![].into())).await?;
-                break;
-            }
-        };
-
-        match msg.opcode {
-            OpCode::Text | OpCode::Binary => {
-                let mut buf = Vec::<u8>::new();
-
-                xcap::Monitor::all().unwrap()
-                    .first().unwrap()
-                    .capture_image().unwrap()
-                    .write_to(
-                        &mut std::io::Cursor::new(&mut buf),
-                        image::ImageFormat::Png,
-                    )?;
-
-                let file_part = reqwest::multipart::Part::bytes(buf)
-                    .file_name("image.png")
-                    .mime_str("image/png")
-                    .unwrap();
-
-                let form = reqwest::multipart::Form::new().part("image", file_part);
-
-                let res = client.post("http://localhost:8080/screenshot/tobias/alpha")
-                    .multipart(form)
-                    .send()
-                    .await?;
-
-                println!("{res:?}");
-
-            }
-            OpCode::Close => {
-                break;
-            }
-            _ => {}
-        }
+pub fn get_credentials<'a>(code: &'a str, firstname: &'a str, lastname: &'a str) -> Option<(u32, String, String)> {
+    if firstname.is_empty() || lastname.is_empty() || code.len() != 3 {
+        return None;
     }
 
-    Ok(())
+    Some((
+        code.parse::<u32>().ok()?,
+        firstname.to_string(),
+        lastname.to_string(),
+    ))
 }
-*/
--- openbox/src/main.rs
+++ openbox/src/main.rs
@@ -1,48 +1,29 @@
 use iced::executor;
-use iced::widget::{
-    row,
-    button, column, container, progress_bar, text, text_input, Column,
-};
-use iced::subscription::{self, Subscription};
+use iced::widget::{button, row, column, text, text_input, };
+use iced::subscription::Subscription;
 use iced::{
-    alignment::Vertical,
-    Alignment, Application, Command, Element, Length, Settings, Subscription as S,
-    Theme,
+    alignment::Vertical, Alignment, Application, 
+    Command, Element, Length, Settings, Theme,
 };
-
-use fastwebsockets::FragmentCollector;
-use hyper::upgrade::Upgraded;
-use hyper_util::rt::TokioIo;
-
-/*
-use futures::channel::mpsc;
-use futures::sink::SinkExt;
-use futures::stream::StreamExt;
-
-use async_tungstenite::tungstenite;
-*/
 
 fn main() -> iced::Result {
     Franklyn::run(Settings::default())
 }
 
-pub enum Franklyn {
-    Login,
-    Disconnected,
-    Connected(FragmentCollector<TokioIo<Upgraded>>),
+enum Franklyn {
+    Login(String, String, String),
+    Connected(openbox::Data),
 }
 
-pub enum Message {
-    Connected(FragmentCollector<TokioIo<Upgraded>>),
-    Disconnected,
-}
+#[derive(Debug, Clone)]
+enum Message {
+    Ev(openbox::Event),
 
-impl std::fmt::Debug for Message {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            _ => write!(f, "not impl"),
-        }
-    }
+    CodeChanged(String),
+    LastnameChanged(String),
+    FirstnameChanged(String),
+
+    Login(openbox::Data),
 }
 
 impl Application for Franklyn {
@@ -53,7 +34,7 @@ impl Application for Franklyn {
 
     fn new(_flags: ()) -> (Franklyn, Command<Message>) {
         (
-            Franklyn::Login,
+            Franklyn::Login(String::new(), String::new(), String::new()),
             Command::none(),
         )
     }
@@ -63,265 +44,76 @@ impl Application for Franklyn {
     }
 
     fn update(&mut self, msg: Message) -> Command<Message> {
-        Command::none()
-    }
-
-    fn subscription(&self) -> Subscription<Message> {
-        if matches!(self, Franklyn::Login) {
-            Subscription::none()
-        } else {
-
-            Subscription::none()
-        }
-    }
-
-    fn view(&self) -> Element<Message> {
-        let title = text("Franklyn");
-
-        column![title].into()
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-pub struct App {
-    code: String,
-    firstname: String,
-    lastname: String,
-    state: State,
-}
-
-#[derive(Debug, Clone)]
-pub enum InternalMessage {
-    Connected,
-    Disconnected,
-    User(String),
-}
-
-#[derive(Debug)]
-#[allow(clippy::large_enum_variant)]
-pub enum InternalState {
-    Disconnected,
-    Connected(
-        async_tungstenite::WebSocketStream<
-            async_tungstenite::tokio::ConnectStream,
-        >,
-        mpsc::Receiver<InternalMessage>,
-    ),
-}
-
-impl std::fmt::Display for InternalMessage {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            InternalMessage::Connected => write!(f, "Connected successfully!"),
-            InternalMessage::Disconnected => {
-                write!(f, "Connection lost... Retrying...")
-            }
-            InternalMessage::User(message) => write!(f, "{message}"),
-        }
-    }
-}
+            Franklyn::Login(c, f, l) => {
+                match msg {
+                    Message::CodeChanged(cc) => *c = cc,
+                    Message::LastnameChanged(lc) => *l = lc,
+                    Message::FirstnameChanged(fc) => *f = fc,
 
-#[derive(Debug, Clone)]
-struct Connection(mpsc::Sender<InternalMessage>);
-
-impl Connection {
-    pub fn send(&mut self, message: InternalMessage) {
-        self.0
-            .try_send(message)
-            .expect("Send message to franklyn server");
-    }
-}
-
-#[derive(Debug, Clone)]
-enum State {
-    Disconnected,
-    Connected(Connection),
-}
-
-#[derive(Debug, Clone)]
-pub enum Event {
-    Connected(Connection),
-    Disconnected,
-    MessageReceived(InternalMessage),
-}
-
-#[derive(Debug, Clone)]
-pub enum Message {
-    CodeChanged(String),
-    FirstnameChanged(String),
-    LastnameChanged(String),
-    Echo(Event),
-    Login((u32, String, String)),
-}
-
-pub fn connect() -> Subscription<Event> {
-    struct Connect;
-
-    subscription::channel(
-        std::any::TypeId::of::<Connect>(),
-        100,
-        |mut output| async move {
-            let mut state = InternalState::Disconnected;
-
-            loop {
-                match &mut state {
-                    InternalState::Disconnected => {
-                        const FRANKLYN: &str = "ws://localhost:8080/examinee/tobias";
-
-                        match async_tungstenite::tokio::connect_async(FRANKLYN).await {
-                            Ok((ws, _)) => {
-                                let (sender, receiver) = mpsc::channel(100);
-
-                                _ = output
-                                    .send(Event::Connected(Connection(sender)))
-                                    .await;
-
-                                state = InternalState::Connected(ws, receiver);
-                            }
-                            Err(_) => {
-                                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-                                _ = output.send(Event::Disconnected).await;
-                            }
-                        }
-                    }
-                    InternalState::Connected(ws, input) => {
-                        let mut fused_ws = ws.by_ref().fuse();
-
-                        futures::select! {
-                            received = fused_ws.select_next_some() => {
-                                match received {
-                                    Ok(tungstenite::Message::Text(msg)) => {
-                                        _ = output.send(Event::MessageReceived(InternalMessage::User(msg)))
-                                            .await;
-                                    }
-                                    Err(_) => {
-                                        _ = output.send(Event::Disconnected).await;
-                                        state = InternalState::Disconnected;
-                                    }
-                                    Ok(_) => continue,
-                                }
-                            }
-
-                            message = input.select_next_some() => {
-                                let result = ws.send(tungstenite::Message::Text(message.to_string())).await;
-
-                                if result.is_err() {
-                                    _ = output.send(Event::Disconnected).await;
-                                    state = InternalState::Disconnected;
-                                }
-                            }
+                    Message::Login(data) => *self = Franklyn::Connected(data),
+                    Message::Ev(e) => {
+                        if matches!(e, openbox::Event::Disconnected) {
+                            eprintln!("ERROR: disconnected");
                         }
                     }
                 }
             }
-        }
-    )
-}
-
-impl App {
-    fn get_credentials(&self) -> Option<(u32, String, String)> {
-        if self.firstname.is_empty() || self.lastname.is_empty() || self.code.len() != 4 {
-            return None;
+            Franklyn::Connected(_) => {}
         }
 
-        Some((
-            self.code.parse::<u32>().ok()?,
-            self.firstname.clone(),
-            self.lastname.clone(),
-        ))
-    }
-}
 
-impl Application for App {
-    type Message = Message;
-    type Theme = Theme;
-    type Executor = executor::Default;
-    type Flags = ();
-
-    fn new(_flags: ()) -> (Self, Command<Message>) {
-        (
-            App {
-                code: String::new(),
-                firstname: String::new(),
-                lastname: String::new(),
-                state: State::Disconnected,
-            },
-            Command::none(),
-        )
-    }
-
-    fn title(&self) -> String {
-        String::from("Franklyn")
-    }
-
-    fn update(&mut self, msg: Message) -> Command<Message> {
-        match msg {
-            Message::CodeChanged(c) => self.code = c,
-            Message::FirstnameChanged(f) => self.firstname = f,
-            Message::LastnameChanged(l) => self.lastname = l,
-            Message::Echo(ev) => {
-
-            }
-            Message::Login((code, firstname, lastname)) => {
-                println!("{code}");
-            }
-        }
         Command::none()
     }
 
     fn subscription(&self) -> Subscription<Message> {
-        connect().map(Message::Echo)
+        match self {
+            Franklyn::Login(_, _, _) => Subscription::none(),
+            Franklyn::Connected(data)=> openbox::connect(&data).map(Message::Ev),
+        }
     }
 
     fn view(&self) -> Element<Message> {
         let title = text(self.title());
 
-        let credentials_input = {
-            let code_input = text_input("Code", &self.code)
-                .on_input(Message::CodeChanged)
-                .padding(10);
+        let content = match self {
+            Franklyn::Login(code, firstname, lastname) => {
+                let code_input = text_input("Code", &code)
+                    .on_input(Message::CodeChanged)
+                    .padding(10);
 
-            let firstname_input = text_input("Firstname", &self.firstname)
-                .on_input(Message::FirstnameChanged)
-                .padding(10);
+                let firstname_input = text_input("Firstname", &firstname)
+                    .on_input(Message::FirstnameChanged)
+                    .padding(10);
 
-            let lastname_input = text_input("Lastname", &self.lastname)
-                .on_input(Message::LastnameChanged)
-                .padding(10);
+                let lastname_input = text_input("Lastname", &lastname)
+                    .on_input(Message::LastnameChanged)
+                    .padding(10);
 
-            let mut button = button(
-                text("connect")
-                    .height(40)
-                    .vertical_alignment(Vertical::Center),
-            )
-            .padding([0, 20]);
+                let mut button = button(
+                    text("connect")
+                        .height(40)
+                        .vertical_alignment(Vertical::Center),
+                )
+                .padding([0, 20]);
 
-            if let Some(credentials) = self.get_credentials() {
-                button = button.on_press(Message::Login(credentials));
+                if let Some((code, firstname, lastname)) = openbox::get_credentials(&code, &firstname, &lastname) {
+                    button = button.on_press(Message::Login(openbox::Data { code, lastname, firstname }));
+                }
+
+                column![code_input, firstname_input, lastname_input, button]
+                    .spacing(10)
+                    .align_items(Alignment::Center)
             }
-
-            column![code_input, firstname_input, lastname_input, button]
-                .spacing(10)
-                .align_items(Alignment::Center)
+            Franklyn::Connected(data) => {
+                column![
+                    row![text(&data.firstname), text(&data.lastname)],
+                    text("Timer?")
+                ]
+            }
         };
 
-        column![title, credentials_input]
+        column![title, content]
             .width(Length::Fill)
             .height(Length::Fill)
             .padding(20)
@@ -329,4 +121,4 @@ impl Application for App {
             .into()
     }
 }
-*/
+
