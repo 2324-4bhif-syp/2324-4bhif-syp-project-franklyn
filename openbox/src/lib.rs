85be76ad40a0ed2b0755c264bd2c8e32c43ea525
aichingert <aichinger.tobias13@gmail.com>
2024-05-03 17:18:33 +0200

    feat: finished basic ui for input credentials
    #244bfk-128

--- openbox/src/lib.rs
+++ openbox/src/lib.rs
@@ -2,6 +2,7 @@ use std::future::Future;
 
 use bytes::Bytes;
 use anyhow::Result;
+use fastwebsockets::{Frame, OpCode};
 use fastwebsockets::FragmentCollector;
 use http_body_util::Empty;
 use hyper::{header::{CONNECTION, UPGRADE}, upgrade::Upgraded, Request};
@@ -23,7 +24,6 @@ where
 }
 
 pub async fn connect(path: &str) -> Result<FragmentCollector<TokioIo<Upgraded>>> {
-
     let stream = TcpStream::connect(DOMAIN).await?;
 
     let req = Request::builder()
@@ -41,4 +41,56 @@ pub async fn connect(path: &str) -> Result<FragmentCollector<TokioIo<Upgraded>>>
 
     let (ws, _) = fastwebsockets::handshake::client(&SpawnExecutor, req, stream).await?;
     Ok(FragmentCollector::new(ws))
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let mut ws = connect("examinee/tobias").await?;
+    let client = reqwest::Client::new();
+
+    loop {
+        let msg = match ws.read_frame().await {
+            Ok(msg) => msg,
+            Err(e)  => {
+                println!("Error: {}", e);
+                ws.write_frame(Frame::close_raw(vec![].into())).await?;
+                break;
+            }
+        };
+
+        match msg.opcode {
+            OpCode::Text | OpCode::Binary => {
+                let mut buf = Vec::<u8>::new();
+
+                xcap::Monitor::all().unwrap()
+                    .first().unwrap()
+                    .capture_image().unwrap()
+                    .write_to(
+                        &mut std::io::Cursor::new(&mut buf),
+                        image::ImageFormat::Png,
+                    )?;
+
+                let file_part = reqwest::multipart::Part::bytes(buf)
+                    .file_name("image.png")
+                    .mime_str("image/png")
+                    .unwrap();
+
+                let form = reqwest::multipart::Form::new().part("image", file_part);
+
+                let res = client.post("http://localhost:8080/screenshot/tobias/alpha")
+                    .multipart(form)
+                    .send()
+                    .await?;
+
+                println!("{res:?}");
+
+            }
+            OpCode::Close => {
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
--- openbox/src/main.rs
+++ openbox/src/main.rs
@@ -1,56 +1,124 @@
-use openbox::connect;
+use iced::executor;
+use iced::widget::{
+    row,
+    button, column, container, progress_bar, text, text_input, Column,
+};
+use iced::{
+    alignment::Vertical,
+    Alignment, Application, Command, Element, Length, Settings, Subscription,
+    Theme,
+};
 
-use anyhow::Result;
-use fastwebsockets::{Frame, OpCode};
+use fastwebsockets::FragmentCollector;
+use hyper::upgrade::Upgraded;
+use hyper_util::rt::TokioIo;
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<()> {
-    let mut ws = connect("examinee/tobias").await?;
-    let client = reqwest::Client::new();
+fn main() -> iced::Result {
+    App::run(Settings::default())
+}
 
-    loop {
-        let msg = match ws.read_frame().await {
-            Ok(msg) => msg,
-            Err(e)  => {
-                println!("Error: {}", e);
-                ws.write_frame(Frame::close_raw(vec![].into())).await?;
-                break;
-            }
-        };
+pub struct App {
+    code: String,
+    firstname: String,
+    lastname: String,
+    session: Option<FragmentCollector<TokioIo<Upgraded>>>
+}
 
-        match msg.opcode {
-            OpCode::Text | OpCode::Binary => {
-                let mut buf = Vec::<u8>::new();
+#[derive(Debug, Clone)]
+pub enum Message {
+    CodeChanged(String),
+    FirstnameChanged(String),
+    LastnameChanged(String),
+    Login((u32, String, String)),
+}
 
-                xcap::Monitor::all().unwrap()
-                    .first().unwrap()
-                    .capture_image().unwrap()
-                    .write_to(
-                        &mut std::io::Cursor::new(&mut buf),
-                        image::ImageFormat::Png,
-                    )?;
-
-                let file_part = reqwest::multipart::Part::bytes(buf)
-                    .file_name("image.png")
-                    .mime_str("image/png")
-                    .unwrap();
-
-                let form = reqwest::multipart::Form::new().part("image", file_part);
-
-                let res = client.post("http://localhost:8080/screenshot/tobias/alpha")
-                    .multipart(form)
-                    .send()
-                    .await?;
-
-                println!("{res:?}");
-
-            }
-            OpCode::Close => {
-                break;
-            }
-            _ => {}
+impl App {
+    fn get_credentials(&self) -> Option<(u32, String, String)> {
+        if self.firstname.is_empty() || self.lastname.is_empty() || self.code.len() != 4 {
+            return None;
         }
+
+        Some((
+            self.code.parse::<u32>().ok()?,
+            self.firstname.clone(),
+            self.lastname.clone(),
+        ))
+    }
+}
+
+impl Application for App {
+    type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        (
+            App {
+                code: String::new(),
+                firstname: String::new(),
+                lastname: String::new(),
+                session: None::<FragmentCollector<TokioIo<Upgraded>>>,
+            },
+            Command::none(),
+        )
     }
 
-    Ok(())
+    fn title(&self) -> String {
+        String::from("Franklyn")
+    }
+
+    fn update(&mut self, msg: Message) -> Command<Message> {
+        match msg {
+            Message::CodeChanged(c) => self.code = c,
+            Message::FirstnameChanged(f) => self.firstname = f,
+            Message::LastnameChanged(l) => self.lastname = l,
+            Message::Login((code, firstname, lastname)) => {
+
+                println!("{code}");
+            }
+        }
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Message> {
+        let title = text(self.title());
+
+        let credentials_input = {
+            let code_input = text_input("Code", &self.code)
+                .on_input(Message::CodeChanged)
+                .padding(10);
+
+            let firstname_input = text_input("Firstname", &self.firstname)
+                .on_input(Message::FirstnameChanged)
+                .padding(10);
+
+            let lastname_input = text_input("Lastname", &self.lastname)
+                .on_input(Message::LastnameChanged)
+                .padding(10);
+
+            let mut button = button(
+                text("connect")
+                    .height(40)
+                    .vertical_alignment(Vertical::Center),
+            )
+            .padding([0, 20]);
+
+            if let Some(credentials) = self.get_credentials() {
+                button = button.on_press(Message::Login(credentials));
+            }
+
+            column![code_input, firstname_input, lastname_input, button]
+                .spacing(10)
+                .align_items(Alignment::Center)
+        };
+
+
+        column![title, credentials_input]
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .padding(20)
+            .spacing(20)
+            .into()
+    }
 }
