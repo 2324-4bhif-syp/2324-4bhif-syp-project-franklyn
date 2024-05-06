ef03478ad68a8d31066e7377eff8cb530bb7e6f6
LukaCvijic <lukacvijic@outlook.com>
2024-05-06 18:49:29 +0200

    feat: Using multiple threads now to convert the images to videos faster #244bfk-130 To Verify

--- server/src/main/java/at/htl/franklyn/recorder/boundary/VideoResource.java
+++ server/src/main/java/at/htl/franklyn/recorder/boundary/VideoResource.java
@@ -10,6 +10,10 @@ import jakarta.ws.rs.Produces;
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -26,7 +30,7 @@ public class VideoResource {
     String screenshotsPath;
 
     Logger logger = Logger.getLogger(getClass().getName());
-    
+
     @GET
     @Path("/download")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
@@ -45,6 +49,17 @@ public class VideoResource {
             if(targetDirectories == null || targetDirectories.length == 0 ){
                 return Uni.createFrom().item(response.build());
             }
+
+            ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+            for(File file : targetDirectories){
+                executor.execute(() -> {
+                    produceVideo(file.getName());
+                });
+            }
+
+            executor.shutdown();
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
 
             response =  Response
                     .ok()
@@ -79,14 +94,16 @@ public class VideoResource {
             FileOutputStream fos = new FileOutputStream(zipPath);
             ZipOutputStream zipOut = new ZipOutputStream(fos);
 
-            for(File files : targetDirectories){
-                String fileName = files.getName();
+            for(File folder : targetDirectories){
+                String fileName = folder.getName();
 
                 if(fileName.endsWith("zip")){
                     continue;
                 }
 
-                InputStream fis = getVideo(fileName).readEntity(FileInputStream.class);
+                String returnFileName = String.format("%s.mp4", fileName);
+
+                InputStream fis = new FileInputStream(Paths.get(folder.getPath(), returnFileName).toString());
                 ZipEntry zipEntry = new ZipEntry(String.format("%s.mp4", fileName));
                 zipOut.putNextEntry(zipEntry);
 
@@ -200,6 +217,38 @@ public class VideoResource {
 
             File targetDirectory = targetDirectories[0];
 
+            produceVideo(username);
+
+            String returnFileName = String.format("%s.mp4", username);
+            return Response
+                    .ok(new FileInputStream(Paths.get(targetDirectory.getPath(), returnFileName).toString()))
+                    .header("Content-Disposition", "attachment; filename=\"" + returnFileName + "\"")
+                    .build();
+        } catch (Exception e) {
+            logger.warning("Type: " + e.getClass().getSimpleName());
+
+            Arrays.stream(e.getStackTrace())
+                    .filter(traceElement -> traceElement.getClassName().equals(getClass().getName()))
+                    .forEach(t -> logger.warning(t.toString()));
+
+            return Response.serverError().build();
+        }
+    }
+
+    private void produceVideo(String username){
+        try {
+            // Parent-folder
+            File screenshotFolder = new File(screenshotsPath);
+            // Input/Output-folder
+            File[] targetDirectories = screenshotFolder.listFiles((f, name) -> name.equals(username));
+
+            // Return if folder with this user does not exist
+            if(targetDirectories == null || targetDirectories.length == 0 ){
+                return;
+            }
+
+            File targetDirectory = targetDirectories[0];
+
             String inputPath = String.format(
                     "%s/%s-*.png",
                     targetDirectory.getPath(), username
@@ -223,22 +272,12 @@ public class VideoResource {
 
             r.freeMemory();
             r.gc();
-
-            String returnFileName = String.format("%s.mp4", username);
-            return Response
-                    .ok(new FileInputStream(Paths.get(targetDirectory.getPath(), returnFileName).toString()))
-                    .header("Content-Disposition", "attachment; filename=\"" + returnFileName + "\"")
-                    .build();
         } catch (Exception e) {
             logger.warning("Type: " + e.getClass().getSimpleName());
 
             Arrays.stream(e.getStackTrace())
                     .filter(traceElement -> traceElement.getClassName().equals(getClass().getName()))
                     .forEach(t -> logger.warning(t.toString()));
-
-            return Response.serverError().build();
         }
     }
-
-
 }
