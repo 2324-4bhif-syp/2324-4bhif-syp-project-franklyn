724b81fd94433f3095f3e70057ce6e68f4713bc6
HikariTempest <arwed@gmx.at>
2024-06-18 19:54:04 +0200

    feat: added the test-overview route and its component #244bfk-138 In Progress

--- instructor-client/src/app/app.routes.ts
+++ instructor-client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {PatrolModeComponent} from "./components/router-components/patrol-mode/patrol-mode.component";
 import {VideoViewerComponent} from "./components/router-components/video-viewer/video-viewer.component";
 import {MetricsDashboardComponent} from "./components/router-components/metrics-dashboard/metrics-dashboard.component";
+import {TestOverviewComponent} from "./components/router-components/test-overview/test-overview.component";
 
 export const routes: Routes = [
-  {path: "", redirectTo: "patrol-mode", pathMatch: "full"},
+  {path: "", redirectTo: "test-overview", pathMatch: "full"},
+  {path: "test-overview", component: TestOverviewComponent},
   {path: "patrol-mode", component: PatrolModeComponent},
   {path: "video-viewer", component: VideoViewerComponent},
   {path: "metrics-dashboard", component: MetricsDashboardComponent}
---
+++ instructor-client/src/app/components/router-components/test-overview/test-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-overview.component.html',
+  styleUrl: './test-overview.component.css'
+})
+export class TestOverviewComponent {
+
+}
