d7b1e305568ee900f8ff936d1dc8091097e004a8
HikariTempest <arwed@gmx.at>
2024-06-19 08:25:40 +0200

    feat: created the basic layout for the test-overview page #244bfk-138 In Progress

--- instructor-client/src/app/components/router-components/test-overview/test-overview.component.ts
+++ instructor-client/src/app/components/router-components/test-overview/test-overview.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {BaseChartDirective} from "ng2-charts";
+import {ExamineeListComponent} from "../../examinee-lists/examinee-list/examinee-list.component";
+import {PatrolPageExamineeComponent} from "../../examinee-versions/patrol-page-examinee/patrol-page-examinee.component";
 
 @Component({
   selector: 'app-test-overview',
   standalone: true,
-  imports: [],
+  imports: [
+    BaseChartDirective,
+    ExamineeListComponent,
+    PatrolPageExamineeComponent
+  ],
   templateUrl: './test-overview.component.html',
   styleUrl: './test-overview.component.css'
 })
