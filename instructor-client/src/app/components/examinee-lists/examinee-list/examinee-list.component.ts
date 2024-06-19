3fda5fdd7a29589919189a096e01a59e103f8d37
HikariTempest <arwed@gmx.at>
2024-06-19 12:53:08 +0200

    refactor: renamed the examinee-components into entity-components #244bfk-138 In Progress

--- instructor-client/src/app/components/examinee-lists/examinee-list/examinee-list.component.ts
+++ instructor-client/src/app/components/examinee-lists/examinee-list/examinee-list.component.ts
@@ -2,7 +2,7 @@ import {Component, inject} from '@angular/core';
 import {StoreService} from "../../../services/store.service";
 import {distinctUntilChanged, map} from "rxjs";
 import {AsyncPipe} from "@angular/common";
-import {PatrolPageExamineeComponent} from "../../examinee-versions/patrol-page-examinee/patrol-page-examinee.component";
+import {PatrolPageExamineeComponent} from "../../entity-components/patrol-page-examinee/patrol-page-examinee.component";
 
 @Component({
   selector: 'app-examinee-list',
--- instructor-client/src/app/components/router-components/test-overview/test-overview.component.ts
+++ instructor-client/src/app/components/router-components/test-overview/test-overview.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import {BaseChartDirective} from "ng2-charts";
-import {ExamineeListComponent} from "../../examinee-lists/examinee-list/examinee-list.component";
-import {PatrolPageExamineeComponent} from "../../examinee-versions/patrol-page-examinee/patrol-page-examinee.component";
 
 @Component({
   selector: 'app-test-overview',
   standalone: true,
   imports: [
-    BaseChartDirective,
-    ExamineeListComponent,
-    PatrolPageExamineeComponent
+    BaseChartDirective
   ],
   templateUrl: './test-overview.component.html',
   styleUrl: './test-overview.component.css'
