04013ef5712e01a3e24e7caa7d9432b1d9b33854
HikariTempest <arwed@gmx.at>
2024-06-25 11:40:55 +0200

    feat: added a new route to the test-edit-view where the user will be able to edit tests in the future #244bfk-138 In Progress

--- instructor-client/src/app/app.routes.ts
+++ instructor-client/src/app/app.routes.ts
@@ -3,11 +3,32 @@ import {PatrolModeComponent} from "./components/router-components/patrol-mode/pa
 import {VideoViewerComponent} from "./components/router-components/video-viewer/video-viewer.component";
 import {MetricsDashboardComponent} from "./components/router-components/metrics-dashboard/metrics-dashboard.component";
 import {TestOverviewComponent} from "./components/router-components/test-overview/test-overview.component";
+import {EditTestViewComponent} from "./components/router-components/edit-test-view/edit-test-view.component";
 
 export const routes: Routes = [
-  {path: "", redirectTo: "test-overview", pathMatch: "full"},
-  {path: "test-overview", component: TestOverviewComponent},
-  {path: "patrol-mode", component: PatrolModeComponent},
-  {path: "test-overview/video-viewer", component: VideoViewerComponent},
-  {path: "metrics-dashboard", component: MetricsDashboardComponent}
+  {
+    path: "",
+    redirectTo: "test-overview",
+    pathMatch: "full"
+  },
+  {
+    path: "test-overview",
+    component: TestOverviewComponent
+  },
+  {
+    path: "patrol-mode",
+    component: PatrolModeComponent
+  },
+  {
+    path: "test-overview/video-viewer",
+    component: VideoViewerComponent
+  },
+  {
+    path: "test-overview/edit-test-view",
+    component: EditTestViewComponent
+  },
+  {
+    path: "metrics-dashboard",
+    component: MetricsDashboardComponent
+  }
 ];
