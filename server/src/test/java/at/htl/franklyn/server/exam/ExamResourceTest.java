1fdc995a1396304749043842f0ad7f8969697fd6
icqun <jln.schludermann@gmail.com>
2024-10-20 13:14:31 +0200

    feat: implement and test getExamById
    #244bfk-144 To Verify

--- server/src/test/java/at/htl/franklyn/server/exam/ExamResourceTest.java
+++ server/src/test/java/at/htl/franklyn/server/exam/ExamResourceTest.java
@@ -7,18 +7,27 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.jboss.resteasy.reactive.RestResponse;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ExamResourceTest {
     private static final String BASE_URL = "exams";
 
+    private static Exam createdExam;
+
     @Test
+    @Order(1)
     void test_simpleCreateValidExam_ok() {
         // Arrange
         LocalDateTime start = LocalDateTime.now().plusYears(2);
@@ -71,5 +80,39 @@ public class ExamResourceTest {
                 .isEqualTo(title);
         assertThat(exam.getScreencaptureInterval())
                 .isEqualTo(interval);
+
+        createdExam = exam;
+    }
+
+    @Test
+    @Order(2)
+    void test_simpleGetExamById_ok() {
+        // Arrange
+        // created Exam is taken from the post test with @Order(1)
+
+        // Act
+        Response response = given()
+                .basePath(BASE_URL)
+            .when()
+                .get(createdExam.getId().toString());
+
+        // Assert
+        assertThat(response.statusCode())
+                .isEqualTo(RestResponse.StatusCode.OK);
+
+        Exam actualExam = response.then()
+                .log().body()
+                .extract().as(Exam.class);
+
+        assertThat(actualExam)
+                .usingRecursiveComparison()
+                .ignoringFieldsOfTypes(LocalDateTime.class)
+                .isEqualTo(createdExam);
+
+        assertThat(actualExam.getPlannedStart())
+                .isCloseTo(createdExam.getPlannedStart(), within(1, ChronoUnit.MINUTES));
+
+        assertThat(actualExam.getPlannedEnd())
+                .isCloseTo(createdExam.getPlannedEnd(), within(1, ChronoUnit.MINUTES));
     }
 }
